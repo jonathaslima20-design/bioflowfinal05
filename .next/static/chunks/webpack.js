a95b329375d578101116fe8370cf82f0821794b7
duncanerastushether5 <duncanerastushether5@hotmail.com>
2026-05-03 12:38:51 -0300

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b244121918fb24f6"; }
+/******/ 		__webpack_require__.h = function() { return "574909432f57364c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
